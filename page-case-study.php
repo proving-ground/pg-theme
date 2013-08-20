647b1a3e56d2c5d9c11870a4420b8f2c2f2c6349
Kevin Bachman <kbachman@provingground.net>
2013-08-20 15:56:59 -0400

    renamed case study theme file

--- page-case-study.php
+++ page-case-study.php
@@ -4,41 +4,41 @@ Template Name: Case Study
 */
 ?>
 
-<?php get_header(); ?>
 
+			
 			<div id="content" class="clearfix row-fluid">
-
+			
 				<div id="main" class="span12 clearfix" role="main">
 
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
+					
 					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-
+						
 						<header>
-
+							
 							<div class="page-header"><h1><?php the_title(); ?></h1></div>
-
+						
 						</header> <!-- end article header -->
-
+					
 						<section class="post_content">
 							<?php the_content(); ?>
-
+					
 						</section> <!-- end article section -->
-
+						
 						<footer>
-
+			
 							<p class="clearfix"><?php the_tags('<span class="tags">' . __("Tags","bonestheme") . ': ', ', ', '</span>'); ?></p>
-
+							
 						</footer> <!-- end article footer -->
-
+					
 					</article> <!-- end article -->
+					
 
-					<?php comments_template(); ?>
-
-					<?php endwhile; ?>
-
+					
+					<?php endwhile; ?>	
+					
 					<?php else : ?>
-
+					
 					<article id="post-not-found">
 					    <header>
 					    	<h1><?php _e("Not Found", "bonestheme"); ?></h1>
@@ -49,13 +49,12 @@ Template Name: Case Study
 					    <footer>
 					    </footer>
 					</article>
-
+					
 					<?php endif; ?>
-
+			
 				</div> <!-- end #main -->
+    
 
-				<?php //get_sidebar(); // sidebar 1 ?>
-
+    
 			</div> <!-- end #content -->
 
-<?php get_footer(); ?>
