25716ba272499aff461d92c7c35d04b6a76f4784
Kevin Bachman <kbachman@provingground.net>
2013-09-04 09:50:47 -0400

    added pG favicon

--- header.php
+++ header.php
@@ -13,6 +13,8 @@
 		<title><?php wp_title( '|', true, 'right' ); ?></title>
 				
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 				
 		<!-- media-queries.js (fallback) -->
 		<!--[if lt IE 9]>
