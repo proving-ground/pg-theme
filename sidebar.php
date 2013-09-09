b08944c3c7c42215eb58133584d516658d0cedf5
Kevin Bachman <kbachman@provingground.net>
2013-09-09 09:59:51 -0400

    fixed search bar

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,19 @@
 				<div id="sidebar1" class="fluid-sidebar sidebar span4" role="complementary">
-				
+				     <div id="search-form">
+                           <form action="<?php echo home_url( '/' ); ?>" method="get" class="form-stacked">
+                               <fieldset>
+                                 <div class="clearfix">
+                                     <div class="input-append input-prepend">
+                                         <span class="add-on"><i class="icon-search"></i></span>
+                                         <input type="text" name="s" id="search" placeholder="<?php _e("Search","bonestheme"); ?>" value="<?php the_search_query(); ?>" />
+                                         <button type="submit" class="search-button"><?php _e("Search","bonestheme"); ?></button>
+                                     </div>
+                                   </div>
+                               </fieldset>
+                           </form>
+                     </div>
+
+
 					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
 						<?php dynamic_sidebar( 'sidebar1' ); ?>
